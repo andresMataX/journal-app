8cfab95c631887be0c1cb3706aa4d24c129ede38
Jesús Mata <andresmata.c21@gmail.com>
2023-03-11 21:55:02 -0600

    Add moment.JS

--- src/journal/views/NoteView.tsx
+++ src/journal/views/NoteView.tsx
@@ -14,6 +14,7 @@ import {
   startUploadingFiles,
 } from '../../store/journal'
 import { DeleteOutline, UploadOutlined } from '@mui/icons-material'
+import moment from 'moment'
 
 interface Props {}
 
@@ -51,7 +52,7 @@ export const NoteView = ({}: Props) => {
   const dateString = useMemo(() => {
     const newDate = new Date(note!.date)
 
-    return newDate.toUTCString()
+    return moment(newDate).format('LLL')
   }, [note!.date])
 
   const onSaveNote: SubmitHandler<FormData> = (data) => {
